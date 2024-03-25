d3bfdf84f0aa0afa1096300cb211c577e32f10c2
guy-jerome <aaronjamesroberts@gmail.com>
2024-03-25 12:55:05 -0400

    Build out the distance function and distance checker.

--- countingGrid.js
+++ countingGrid.js
@@ -1,5 +1,5 @@
 // Task: Given a 2D array, an (n) distance, and an array of [x,y] positive cell arrays
-// Print out how many positive neighboring cells are found within the manhattan distance threshhold of the original positive cells.
+// Print out how many positive neighboring cells are found within the manhattan distance threshold of the original positive cells.
 // example: X is the original positive cell. 0 = negative; 1 = positive. n = 2; There are 13 positive cells - 12 neighbors (1), 1 original (X)
 // 0 0 1 0 0
 // 0 1 1 1 0
@@ -10,25 +10,36 @@
 // Hint: Manhattan distance: [x1 - x2] + [y1 - y2]
 
 // Refer back to the grid cell neighborhood PDF for more details, assumptions, and examples
-// Please do not hesistate to reach out with an questions!
+// Please do not hesitate to reach out with an questions!
 
 // Parameters
 // collXCount: number - number of columns
 // rowYCount: number - number of rows
 // n: number - distance threshold
 // positiveCellsXYArray: Array<Array>> - array of [x,y] arrays. Ex: [[1,3], [5,5], [5,8]]
+function getManhattanDistance(XYArray1, XYArray2) {
+  return (
+    Math.abs(XYArray1[0] - XYArray2[0]) + Math.abs(XYArray1[1] - XYArray2[1])
+  );
+}
+
+function checkInRange(XYArray1, XYArray2, n) {
+  const distance = getManhattanDistance(XYArray1, XYArray2);
+  return distance <= n ? true : false;
+}
+
 function main(collXCount, rowYCount, n, positiveCellsXYArray) {
-  var count = 0;
+  let count = 0;
 
   // *** Implement this function ***
-
-  console.log("total count of positive cells is: ", count);
 }
 
 // *** General test cases - feel free to expand and add more (we will be adding more when testing your code) ***
-main(10, 10, 3, [[5, 5]]); //will return 25
-main(10, 10, 2, [
-  [7, 3],
-  [3, 7],
-]); //will return 26
-main(10, 10, 3, [[2, 2]]); //will return 23
+// main(10, 10, 3, [[5, 5]]); //will return 25
+// main(10, 10, 2, [
+//   [7, 3],
+//   [3, 7],
+// ]); //will return 26
+// main(10, 10, 3, [[2, 2]]); //will return 23
+
+module.exports = main;
---
+++ countingGrid.test.js
@@ -0,0 +1,18 @@
+const main = require("./countingGrid");
+
+describe("Counting Grid Test", () => {
+  test("A single positive value in the center of the grid", () => {
+    expect(main(10, 10, 3, [[5, 5]])).toBe(25);
+  }),
+    test("Two positive value in the center of the grid overlapping", () => {
+      expect(
+        main(10, 10, 2, [
+          [7, 3],
+          [3, 7],
+        ])
+      ).toBe(26);
+    }),
+    test("A positive value on the edge of the grid", () => {
+      expect(main(10, 10, 3, [[2, 2]])).toBe(23);
+    });
+});
