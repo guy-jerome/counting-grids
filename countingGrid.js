aee52b66edf7276acb29e8647891404da73a311a
guy-jerome <aaronjamesroberts@gmail.com>
2024-03-25 19:44:53 -0400

    Adds some simple comments

--- countingGrid.js
+++ countingGrid.js
@@ -1,20 +1,24 @@
+// Calculates the Manhattan distance between two points given as arrays [x, y]
 function getManhattanDistance(XYArray1, XYArray2) {
   return (
     Math.abs(XYArray1[0] - XYArray2[0]) + Math.abs(XYArray1[1] - XYArray2[1])
   );
 }
 
+// Checks if the Manhattan distance between two points is less than or equal to n
 function checkInRange(XYArray1, XYArray2, n) {
   const distance = getManhattanDistance(XYArray1, XYArray2);
   return distance <= n ? true : false;
 }
 
+// Represents a matrix with methods for manipulation and checking
 class Matrix {
   constructor(collXCount, rowYCount, defaultValue = 0) {
     this.collXCount = collXCount;
     this.rowYCount = rowYCount;
     this.defaultValue = defaultValue;
     this.matrix = [];
+    // Generates the Matrix
     for (let i = 0; i < this.collXCount; i++) {
       let column = [];
       for (let j = 0; j < this.rowYCount; j++) {
@@ -23,21 +27,19 @@ class Matrix {
       this.matrix.push(column);
     }
   }
-  getMatrix() {
-    return this.matrix;
-  }
-  printMatrix() {
-    const numRows = this.matrix.length;
-    const numCols = this.matrix[0].length;
 
-    for (let col = 0; col < numCols; col++) {
+  // Prints the matrix to the console
+  printMatrix() {
+    for (let col = 0; col < this.collXCount; col++) {
       let rowData = [];
-      for (let row = 0; row < numRows; row++) {
+      for (let row = 0; row < this.rowYCount; row++) {
         rowData.push(this.matrix[row][col]);
       }
       console.log(rowData.join());
     }
   }
+
+  // Returns the count of positive values in the matrix
   getPositive() {
     return this.matrix.reduce((count, row) => {
       return (
@@ -48,9 +50,13 @@ class Matrix {
       );
     }, 0);
   }
+
+  // Sets a cell in the matrix to a positive value
   setCellToPositive(XYArray) {
     this.matrix[XYArray[0]][XYArray[1]] = 1;
   }
+
+  // Checks if a given cell is inside the matrix
   checkIfInside(XYArray) {
     if (XYArray[0] < 0 || XYArray[0] >= this.collXCount) {
       return false;
@@ -59,6 +65,8 @@ class Matrix {
     }
     return true;
   }
+
+  // Applies the brute-force positive cell filling algorithm to the matrix
   bruteForceCheck(positiveCellsXYArray, n) {
     for (let array of positiveCellsXYArray) {
       this.setCellToPositive(array);
@@ -71,6 +79,8 @@ class Matrix {
       }
     }
   }
+
+  // Applies the adjacent walk positive cell filling algorithm to the matrix
   adjacentWalkCheck(positiveCellsXYArray, n) {
     for (let array of positiveCellsXYArray) {
       this.setCellToPositive(array);
@@ -105,6 +115,7 @@ class Matrix {
   }
 }
 
+// Main function to run the algorithm
 function main(collXCount, rowYCount, n, positiveCellsXYArray) {
   // Parameters
   // collXCount: number - number of columns
@@ -113,7 +124,16 @@ function main(collXCount, rowYCount, n, positiveCellsXYArray) {
   // positiveCellsXYArray: Array<Array>> - array of [x,y] arrays. Ex: [[1,3], [5,5], [5,8]]
   const grid = new Matrix(collXCount, rowYCount);
   grid.adjacentWalkCheck(positiveCellsXYArray, n);
+  grid.printMatrix();
   return grid.getPositive();
 }
+
+// Example usage
+main(10, 10, 3, [[5, 5]]); //will return 25
+main(10, 10, 2, [
+  [7, 3],
+  [3, 7],
+]); //will return 26
+main(10, 10, 3, [[2, 2]]); //will return 23
 
 module.exports = main;
